1a7fd48fbd53798c879a9c354035e94435e18694
Cedric Gatay <c.gatay@code-troopers.com>
2017-04-14 14:47:41 +0200

    feat(slackPresence): Slack Presence button
    Slack presence button behavior

--- app.js
+++ app.js
@@ -2,26 +2,38 @@ var dash_button = require('node-dash-button');
 var request = require('request');
 var debug = 0;
 var macAdress=process.env.DASH_MAC;
-var auth=process.env.AUTH;
-var url=process.env.URL;
-var method=process.env.METHOD;
-console.log("Started dash button listener app", macAdress, auth, url, method);
+var msg=process.env.MSG;
+var emoticon=process.env.EMOTICON;
+var token=process.env.TOKEN;
+var duration=parseInt(process.env.DURATION || 5, 10);
+
+var callEndpoint = function(m, e){
+    var profileObj = {
+        'status_text': m,
+        'status_emoji': e
+    };
+    var url = 'https://slack.com/api/users.profile.set?token='+token+'&profile=' + encodeURIComponent(JSON.stringify(profileObj));
+    if (debug === 1){
+        console.log(url);
+        console.log(JSON.stringify(profileObj))
+    }
+    request({
+            url: url,
+            method: 'POST'
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                if (debug === 1) {console.log((new Date()) + "Got response Value: " + response.statusCode);}
+            }else{
+                console.log((new Date()) + " - Error : "  + error + " - Status Code:" + (response ? response.statusCode : 'no response'));
+            }
+        });
+};
+console.log("Started dash button listener app", macAdress, token);
 // ac:63:be:a4:5e:6b
 var dash = dash_button(macAdress, null, null, "all");
 dash.on("detected", function (btn){
-  console.log("Button found with mac address :", btn);
-  request({
-    url: url,
-    method: method,
-    headers : {
-      "Authorization" : "Basic " + auth
-    }
-  },
-  function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      if (debug == 1) {console.log((new Date()) + "Got response Value: " + response.statusCode);}
-    }else{
-      console.log((new Date()) + " - Error : "  + error + " - Status Code:" + response.statusCode);
-    }
-  });
+    console.log("Button found with mac address :", btn);
+    callEndpoint(msg,emoticon);
+    setTimeout(function(){callEndpoint('','');}, duration * 60 * 1000);
 });
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-for btn in `ls *.env -1 | grep -v sample`; do 
+#!/usr/bin/env bash
+for btn in `ls *.env -1 | grep -v sample`; do
 	NAME=`basename -s .env $btn`; 
 	echo "Configuring $NAME";
 	docker rm -fv dash-$NAME
-	docker run -d --name=dash-$NAME --restart=always --net=host --env-file $NAME.env cgatay/dash-button-rpi
+	docker run -d --name=dash-$NAME --restart=always --net=host --env-file $NAME.env cgatay/dash-button-slack
 done;
